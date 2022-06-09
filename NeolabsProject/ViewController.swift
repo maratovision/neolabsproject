4fa2fb3ce2a19a299f3cec0c3dc71fb3c6466a40
Beks <beks@MacBook-Pro-Beks.local>
2022-06-10 00:27:50 +0600

    made a calculator and other things

--- NeolabsProject/ViewController.swift
+++ NeolabsProject/ViewController.swift
@@ -8,10 +8,71 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var numberFromScreen: Double = 0;
+    var firstNumFromScreen: Double = 0;
+    var operation: Int = 0;
+    var mathSign: Bool = false
 
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func digits(_ sender: UIButton) {
+        if mathSign == true {
+            result.text = String(sender.tag)
+            mathSign = false
+        }
+        else{
+            result.text = result.text! + String(sender.tag)
+        }
+        
+        numberFromScreen = Double(result.text!)!
+        
+    }
+    
+    @IBAction func buttons(_ sender: UIButton) {
+        if result.text != "" && sender.tag != 15 && sender.tag != 10{
+            firstNumFromScreen = Double(result.text!)!
+            
+            if sender.tag == 11{
+                result.text = "/"
+            }
+            else if sender.tag == 12{
+                result.text = "x"
+            }
+            else if sender.tag == 13{
+                result.text = "-"
+            }
+            else if sender.tag == 14{
+                result.text = "+"
+            }
+            
+            operation = sender.tag
+            mathSign = true;
+        }
+        else if sender.tag == 15{
+            if operation == 11{
+                result.text = String(firstNumFromScreen / numberFromScreen)
+            }
+            else if operation == 12{
+                result.text = String(firstNumFromScreen * numberFromScreen)
+            }
+            else if operation == 13{
+                result.text = String(firstNumFromScreen - numberFromScreen)
+            }
+            else if operation == 14{
+                result.text = String(firstNumFromScreen + numberFromScreen)
+            }
+        }
+        else if sender.tag == 10{
+            result.text = ""
+            firstNumFromScreen = 0
+            numberFromScreen = 0
+            operation = 0
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.==
     }
 
 
