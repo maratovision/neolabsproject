d3b2e49c6fb5803a9a9b74ede2d678708361ff31
Beks <beks@MacBook-Pro-Beks.local>
2022-06-09 19:12:47 +0600

    first commit

---
+++ NeolabsProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NeolabsProject
+//
+//  Created by Beks on 9/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.==
+    }
+
+
+}
+
